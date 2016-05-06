6a2d669170735d02e4b72944fae22289d3ce2859
DengZuoheng <dengzuoheng@gmail.com>
2016-05-06 18:28:57 +0800

    build.sh fin

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
-apt-get update
-sudo apt-get install python-dev python-pip libxml2-dev libxslt1-dev zlib1g-dev libffi-dev libssl-dev
-apt-get install -y python-pip
+sudo apt-get install -y python-dev python-pip libxml2-dev libxslt1-dev zlib1g-dev libffi-dev libssl-dev
 apt-get install -y redis-server
 apt-get install -y mongodb
 pip install pymongo
 pip install Scrapy
 pip install redis
-pip install numpy
+#pip install numpy
 pip install scrapy-crawlera
