e691312e1132ef6370f2745e7163ff02d9748bce
DengZuoheng <dengzuoheng@gmail.com>
2016-05-06 17:49:16 +0800

    update install script of scrapy

--- build.sh
+++ build.sh
@@ -1,13 +1,10 @@
 apt-get update
+sudo apt-get install python-dev python-pip libxml2-dev libxslt1-dev zlib1g-dev libffi-dev libssl-dev
 apt-get install -y python-pip
 apt-get install -y redis-server
 apt-get install -y mongodb
-apt-get install -y python-dev
-apt-get install -y libxml2
-apt-get install -y libxml2-dev
-apt-get install -y libxslt-dev
 pip install pymongo
-pip install scrapy
+pip install Scrapy
 pip install redis
 pip install numpy
 pip install scrapy-crawlera
