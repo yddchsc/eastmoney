00d3e32f2a71110335ab78a438a8229e61af69f5
DengZuoheng <dengzuoheng@gmail.com>
2016-05-06 16:48:27 +0800

    add libxml2 etc

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@ apt-get install -y python-pip
 apt-get install -y redis-server
 apt-get install -y mongodb
 apt-get install -y python-dev
+apt-get install -y libxml2
+apt-get install -y libxml2-dev
+apt-get install -y libxslt-dev
 pip install pymongo
 pip install scrapy
 pip install redis
