4bd47d3b3c7da7344ecda65f5c87be09662a4ea2
DengZuoheng <dengzuoheng@gmail.com>
2016-05-06 16:31:03 +0800

    finxed build.sh install mongodb in aliyun ecs

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 apt-get update
 apt-get install -y python-pip
 apt-get install -y redis-server
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
-apt-get update
-apt-get install -y mongodb-10gen
+apt-get install -y mongodb
 apt-get install -y python-dev
 pip install pymongo
 pip install scrapy
